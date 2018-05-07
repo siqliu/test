27869d50e76ed15c74acfc2d55eabb770aa30c06
siqliu <siqliu@thoughtworks.com>
2018-05-07 18:13:08 +0800

    test

--- src/test/java/com/example/employee/EmployeeJPATest.java
+++ src/test/java/com/example/employee/EmployeeJPATest.java
@@ -17,8 +17,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
@@ -36,66 +34,5 @@ public class EmployeeJPATest {
         flyway.setDataSource("jdbc:mysql://localhost:3306/employee_db","root","root");
         flyway.clean();
         flyway.migrate();
-    }
-
-    @Test
-    public void should_return_employee_when_input_employee_name() {
-        //1.查询名字是小红的employee
-        String actualName = "xiaohong";
-        Employee expectedEmployee = employeeRepository.findByName(actualName);
-        assertThat(actualName).isEqualTo(expectedEmployee.getName());
-    }
-
-    @Test
-    public void should_return_employee_given_character_in_name_and_salary_large_than() {
-        //2.找出Employee表中第一个姓名包含`n`字符的雇员所有个人信息
-        //此处补充测试：且工资大于6000
-        String actualName = "xiaohong";
-        Employee expectedEmployee = employeeRepository.findByNameContainsAndSalaryGreaterThan("n", 6000);
-        assertThat(actualName).isEqualTo(expectedEmployee.getName());
-        System.out.println(expectedEmployee.getName() + ":" + expectedEmployee.getSalary());
-        actualName = "xiaozhi";
-        expectedEmployee = employeeRepository.findByNameContainsAndSalaryGreaterThan("x", 7000);
-        assertThat(actualName).isEqualTo(expectedEmployee.getName());
-    }
-
-    @Test
-    public void should_return_employee_name_when_employee_salary_is_max_and_given_company_id_() {
-        //3.找出一个薪资最高且公司ID是1的雇员以及该雇员的name
-        Employee expectedEmployee = employeeRepository.findFirstByCompanyIdEqualsOrderBySalaryDesc(1);
-        String actualName = "xiaohong";
-        assertThat(actualName).isEqualTo(expectedEmployee.getName());
-    }
-
-    @Test
-    public void should_return_employee_list_when_input_page_request() {
-        //4.实现对Employee的分页查询，每页两条数据，一共三页数。
-        //注意：PageRequest的构造方法已经弃用了代替的是PageRequest.of,并且最后一个参数代表按照table中的哪一个字段排序
-        Page<Employee> EmployeePage = employeeRepository.findAll(PageRequest.of(3, 2));
-        assertThat(EmployeePage.getTotalPages()).isEqualTo(3);
-    }
-
-    @Test
-    public void should_return_company_name_when_input_employee_name() {
-        //5.查找xiaohong的所在的公司的公司名称
-        String expectedCompanyName = "alibaba";
-        String actualCompanyName = employeeRepository.findCompanyByEmployeeName("xiaohong");
-        assertThat(actualCompanyName).isEqualTo(expectedCompanyName);
-    }
-
-    @Test
-    public void should_return_influence_lines_when_update_employee_name() {
-        //6.将xiaohong的名字改成xiaobai,输出这次修改影响的行数
-        Integer expectedLine = 1;
-        Integer actualLine = employeeRepository.modifyNameReturnRows("xiaobai", "xiaohong");
-        assertThat(actualLine).isEqualTo(expectedLine);
-    }
-
-    @Test
-    public void should_deleted_employee_when_given_employee_name() {
-        //7.删除姓名是xiaohong的employee
-        employeeRepository.deleteEmployeeByName("xiaohong");
-        Employee actualEmployee = employeeRepository.findByName("xiaohong");
-        assertThat(actualEmployee).isNull();
     }
 }
